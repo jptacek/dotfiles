9270af03e8daa0ca22abcca5c735867deff36379
John Ptacek <jptacek@gmail.com>
2019-04-29 08:55:38 -0500

    Macos installs for applications. Add NPM global installs. Add VSCode extension installs

---
+++ packages/npmGlobal.sh
@@ -0,0 +1,7 @@
+# Global NPM packages for new machine
+
+npm install create-react-app -g
+npm install expo -g
+npm install expo-cli -g
+npm install gatsby-cli -g 
+#npm install docpad -g
---
+++ packages/vsCode.sh
@@ -0,0 +1,13 @@
+# Install VS code things i like
+
+code --install-extension dbaeumer.vscode-eslint
+
+code --install-extension CoenraadS.bracket-pair-colorizer
+
+code --install-extension esbenp.prettier-vscode
+
+code --install-extension ms-python.python
+
+code --install-extension eamodio.gitlens
+
+code --install-extension DavidAnson.vscode-markdownlint
