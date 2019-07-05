b49cdebeb77b112334c3a2594917a61e17a1d218
John Ptacek <john.ptacek@breakthroughfuel.com>
2019-07-05 07:27:24 -0500

    Add tmux to MacOS install

--- macos/brewInstall.sh
+++ macos/brewInstall.sh
@@ -1,29 +1,43 @@
+echo "Installing Firefox"
 brew cask install firefox
 
+echo "Installing iTerm2"
 brew cask install iterm2
 
+echo "Installing VS Code"
 brew cask install visual-studio-code
 
+echo "Installing Postman"
 brew cask install postman
 
+echo "Installing Brave browser"
 brew cask install brave-browser
 
+echo "Installing slack"
 brew cask install slack
 
+echo "Installing Skype"
 brew cask install skype
 
+echo "Installing Spotify"
 brew cask install spotify
 
+echo "Installing Insomnia"
 brew cask install insomnia
 
+echo "Installing Figman"
 brew cask install figma
 
+echo "Installing noefetch"
 brew install neofetch
 
+echo "Installing Tmux"
 brew install tmux
 
 # Azure data studio with Postgress support
+echo "Installing Azure Data Studio"
 brew cask install azure-data-studio
 
 # Google cloud SDk, required Python 2.7
+echo "Installing Google SDK"
 brew cask install google-cloud-sdk
